32e155efc1d2c6b302c425cf398dc7be9f91dcef
Prima Aulia Gusta <primaulia@gmail.com>
2017-06-07 12:35:25 +0800

    stabilize navi animation

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,4 +4,17 @@
   var vpw = $(window).width()
   var vph = $(window).height()
   // alert(vpw)
+
+  var $menuOpener = $('.menu-opener')
+
+  $menuOpener.on('click', function () {
+    var $navAbsolute = $(this).parents('header').find('.nav-absolute')
+    var $allAs = $(this).parents('.nav-pointer').find('a')
+
+    $.each($allAs, function (index, elem) {
+      $(elem).toggleClass('changecolor')
+    })
+    $(this).css('background-color', )
+    $navAbsolute.toggleClass('popdown')
+  })
 }($))
